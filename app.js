aff099aa9f37bf8fd6303cb73a9da34205954147
Junyong-Ahn <junyongsw92@gmail.com>
2021-07-06 23:47:50 +0900

    Apply default formatter - Prettier

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const loginForm = document.querySelector('#login-form');
-const loginInput = loginForm.querySelector('#login-form input');
-const greeting =document.querySelector('#greeting');
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = 'username';
+const USERNAME_KEY = "username";
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-function onLoginSubmit(event){
-    event.preventDefault();
-    // lolcalstorage에 username이 있으면
-    loginForm.classList.add(HIDDEN_CLASSNAME); //loginform 숨기기
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  // lolcalstorage에 username이 있으면
+  loginForm.classList.add(HIDDEN_CLASSNAME); //loginform 숨기기
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-if(!savedUsername){
-    // show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener('submit', onLoginSubmit);
-}else{
-    // show the greetings
-    paintGreetings(savedUsername);
+if (!savedUsername) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUsername);
 }
