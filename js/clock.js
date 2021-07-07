393619fe4851e0f04070af2a2287d659f893a0f3
Junyong-Ahn <junyongsw92@gmail.com>
2021-07-07 23:10:33 +0900

    Create Clock

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${hours} : ${minutes} : ${seconds}`;
+}
+
+// setInterval(sayHello, 5000);
+// setTimeout(sayHello, 3000);
+getClock(); // 화면이 로딩되고 바로 시을 띄우기 위함
+setInterval(getClock, 1000);
