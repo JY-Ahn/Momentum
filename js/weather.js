11c6012fa11f75b87a4981ebaab5c6b22408bbea
Junyong-Ahn <junyongsw92@gmail.com>
2021-07-18 21:59:48 +0900

    Complete weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,20 @@
-const API_KEY = "2be821d24b7893d7a82d02fa403eff26";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "7df3f80836e96bb3163f8addacf24af6";
 
 function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=matric`;
-
-  //   console.log(url);
-  //   console.log("You live in", lat, lon);
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=matric`;
+  console.log(url);
   fetch(url)
-    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
-
-      weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
-      city.innerHTML = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
     });
 }
 
